30e7b00eae8d5469087dd1c3b5612314683aa4c2
aakashmak <49230573+aakashmak@users.noreply.github.com>
2026-03-10 12:34:09 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name='clinicalmind',
+    version='0.1',
+    author="Aakash",
+    author_email="aakashmak1809@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
